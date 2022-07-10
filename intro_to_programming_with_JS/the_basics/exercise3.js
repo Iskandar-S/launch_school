c80eac6eab9a3c32f53e91c99588addbc35e6f98
Iskandar S <isalokhiddinov@gmail.com>
2022-07-10 09:46:25 -0400

    finished exercise 3

---
+++ intro_to_programming_with_JS/the_basics/exercise3.js
@@ -0,0 +1,13 @@
+let value1 = typeof 'true';
+
+let value2 = typeof false;
+
+let value3 = typeof 1.5;
+
+let value4 = typeof 2;
+
+let value5 = typeof undefined;
+
+let value6 = typeof { foo: 'bar' };
+
+console.log(value1, value2, value3, value4, value5, value6);
