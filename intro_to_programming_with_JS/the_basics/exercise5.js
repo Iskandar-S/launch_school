f1f51beb4cebe08bdf8c89c502838f665fc45eb2
Iskandar S <isalokhiddinov@gmail.com>
2022-07-10 10:02:19 -0400

    finished exercise 5

---
+++ intro_to_programming_with_JS/the_basics/exercise5.js
@@ -0,0 +1,12 @@
+console.log(Number('5') + 10);
+
+// or
+
+console.log(parseInt('5') + 10);
+
+// Solution:
+
+// console.log(parseInt('5', 10) + 10);
+
+// Second argument was for the base. Interesting and confusing a little.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
