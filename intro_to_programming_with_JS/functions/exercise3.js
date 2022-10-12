b227c27038bf5657256549a2b4d912b68e72a778
Iskandar <30454668+Iskandar-S@users.noreply.github.com>
2022-10-11 21:45:04 -0400

    finished exercise 3

---
+++ intro_to_programming_with_JS/functions/exercise3.js
@@ -0,0 +1,11 @@
+function getNumber(prompt) {
+	let rlSync = require('readline-sync');
+	let number = rlSync.question(prompt);
+	return number;
+}
+
+let firstNumber = Number(getNumber('Enter the first number: '));
+let secondNumber = Number(getNumber('Enter the second number: '));
+let result = firstNumber * secondNumber;
+
+console.log(`${firstNumber} * ${secondNumber} = ${result}`);
