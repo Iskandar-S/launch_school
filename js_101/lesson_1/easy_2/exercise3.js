824107cd7b75e163147a1a57008fadb9d9573713
Iskandar S <30454668+Iskandar-S@users.noreply.github.com>
2023-09-23 20:23:51 -0400

    finished exercise 3

---
+++ js_101/lesson_1/easy_2/exercise3.js
@@ -0,0 +1,13 @@
+// Problem:
+
+// Create a function that takes two arguments, multiplies them together, and returns the result.
+
+// Example:
+
+console.log(multiply(5, 3) === 15); // logs true
+
+// Solution:
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
