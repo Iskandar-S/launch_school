daf0c4f51af4446e1d01595f5384e5e9cb3e5736
Iskandar S <30454668+Iskandar-S@users.noreply.github.com>
2023-09-28 22:53:51 -0400

    finished exercise 5

---
+++ js_101/lesson_1/easy_3/exercise5.js
@@ -0,0 +1,33 @@
+// Problem:
+
+// Write a function that takes a positive integer, n, as an argument and logs a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below) should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+// Examples:
+
+triangle(5);
+
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+triangle(9);
+
+//         *
+//        **
+//       ***
+//      ****
+//     *****
+//    ******
+//   *******
+//  ********
+// *********
+
+// Solution:
+
+function triangle(n) {
+  for (let i = 1; i <= n; i += 1) {
+    console.log('*'.repeat(i).padStart(n, ' '));
+  }
+}
